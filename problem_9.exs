a56dfb4faa44a9016fe4859db7dcff9712726ad4
Nick Bugaiov <nick@bugaiov.com>
2022-04-02 13:16:00 +0200

    problem 9

---
+++ problem_9.exs
@@ -0,0 +1,23 @@
+defmodule Problem9 do
+  def inner_loop(s, _a, b \\ 2)
+  def inner_loop(s, _a, b) when b == div(s, 2), do: nil
+  def inner_loop(s, a, b) when b < div(s, 2) do
+    case s - a - b == :math.sqrt(a*a + b*b) do
+      true -> [ a, b, s - a - b ]
+      false -> inner(s, a, b + 1)
+    end
+  end
+
+  def triplet(s, a \\ 1, b \\ 2)
+  def triplet(s, a, b) when a == div(s, 3) and b == div(s, 2), do: nil
+  def triplet(s, a, b) when a < div(s, 3) and b < div(s, 2) do
+    case inner_loop(s, a, b) do
+      h = [ _, _, _ ] -> h
+      _ -> triplet(s, a + 1, b + 1)
+    end
+  end
+
+  def triplet_mult(s), do: triplet(s) |> Enum.reduce(1, fn n, acc -> n * acc end)
+end
+
+IO.inspect Problem9.triplet_mult(1000)
